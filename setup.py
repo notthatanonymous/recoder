73ec5032f33c485138b84cd6ae4434e8d300d546
Ahmad Abdellatif <124534399+notthatanonymous@users.noreply.github.com>
2023-10-20 21:03:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ import recoder
 setup(
   name='recsys-recoder',
   version=recoder.__version__,
-  install_requires=['torch==1.8.1', 'annoy==1.17.0',
-                    'numpy==1.19.5', 'scipy>=1.5.4',
+  install_requires=['torch', 'annoy==1.17.0',
+                    'numpy', 'scipy>=1.5.4',
                     'tqdm==4.59.0', 'glog==0.3.1'],
   packages=find_packages(),
   author='Abdallah Moussawi',
